858bdd21b6de03552228b5265317d38987159161
Solzanir Souza <souzanirs@gmail.com>
2021-03-13 09:34:37 -0300

    commit inicial

---
+++ src/main/java/souza/solzanir/georreferencia/GeorreferenciaApplication.java
@@ -0,0 +1,13 @@
+package souza.solzanir.georreferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorreferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorreferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/souza/solzanir/georreferencia/GeorreferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package souza.solzanir.georreferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeorreferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
